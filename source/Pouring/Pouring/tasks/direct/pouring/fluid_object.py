454a6ad4d58c1f3f0d4ab1734667d41c2b152bc3
robegi <robertogiglio2000@gmail.com>
2025-10-08 10:49:13 +0200

    Modifies fluid object functions to handle tensors on the gpu

--- source/Pouring/Pouring/tasks/direct/pouring/fluid_object.py
+++ source/Pouring/Pouring/tasks/direct/pouring/fluid_object.py
@@ -6,8 +6,8 @@ import omni.timeline
 import numpy as np
 import omni.kit.commands
 import torch
+from typing import Union
 
-from isaacsim.core.api import SimulationContext
 
 class FluidObjectCfg():
 
@@ -39,12 +39,14 @@ class FluidObject():
         self.stage.SetDefaultPrim(self.default_prim)
         self.default_prim_path = self.stage.GetDefaultPrim().GetPath()
         self.scenePath = Sdf.Path("/physicsScene")
+
+        # simcontext = SimulationContext.instance()
         
     
     def spawn_fluid_direct(self, env_index: int = 0):
             
-            # Particle System
-            self.particleSystemPath = Sdf.Path(f"/World/envs/env_{env_index}").AppendChild("particleSystem")
+             # Particle System
+            self.particleSystemPath = self.default_prim_path.AppendChild("particleSystem")
 
             # Particle points
             self.particlesPath = Sdf.Path(f"/World/envs/env_{env_index}/particles")
@@ -70,21 +72,65 @@ class FluidObject():
                 solver_position_iterations=self._solverPositionIterations,
             )
 
-            # Set particle system properties
-            material_path = omni.usd.get_stage_next_free_path(self.stage, "/pbdParticleMaterial", True)
-            particleUtils.add_pbd_particle_material(self.stage, material_path)
+            mtl_created = []
             omni.kit.commands.execute(
-                "BindMaterialCommand",
-                prim_path=self.stage.GetPrimAtPath(self.particleSystemPath).GetPath(),
-                material_path=material_path,
-                strength=None,
+                "CreateAndBindMdlMaterialFromLibrary",
+                mdl_name="OmniSurfacePresets.mdl",
+                mtl_name="OmniSurface_DeepWater",
+                mtl_created_list=mtl_created,
             )
+            pbd_particle_material_path = mtl_created[0]
+            omni.kit.commands.execute(
+                "BindMaterial", prim_path=self.particleSystemPath, material_path=pbd_particle_material_path
+            )
+
+            # Create a pbd particle material and set it on the particle system
+            particleUtils.add_pbd_particle_material(
+                self.stage,
+                pbd_particle_material_path,
+                cohesion=10,
+                viscosity=self.cfg.viscosity,
+                surface_tension=0.74,
+                friction=0.1,
+            )
+            physicsUtils.add_physics_material_to_prim(self.stage, particle_system.GetPrim(), pbd_particle_material_path)
 
             particle_system.CreateMaxVelocityAttr().Set(200)
 
+            # # add particle anisotropy
+            # anisotropyAPI = PhysxSchema.PhysxParticleAnisotropyAPI.Apply(particle_system.GetPrim())
+            # anisotropyAPI.CreateParticleAnisotropyEnabledAttr().Set(True)
+            # aniso_scale = 5.0
+            # anisotropyAPI.CreateScaleAttr().Set(aniso_scale)
+            # anisotropyAPI.CreateMinAttr().Set(1.0)
+            # anisotropyAPI.CreateMaxAttr().Set(2.0)
+
+            # # add particle smoothing
+            # smoothingAPI = PhysxSchema.PhysxParticleSmoothingAPI.Apply(particle_system.GetPrim())
+            # smoothingAPI.CreateParticleSmoothingEnabledAttr().Set(True)
+            # smoothingAPI.CreateStrengthAttr().Set(0.5)
+
+            # # apply isosurface params
+            # isosurfaceAPI = PhysxSchema.PhysxParticleIsosurfaceAPI.Apply(particle_system.GetPrim())
+            # isosurfaceAPI.CreateIsosurfaceEnabledAttr().Set(True)
+            # isosurfaceAPI.CreateMaxVerticesAttr().Set(1024 * 1024)
+            # isosurfaceAPI.CreateMaxTrianglesAttr().Set(2 * 1024 * 1024)
+            # isosurfaceAPI.CreateMaxSubgridsAttr().Set(1024 * 4)
+            # isosurfaceAPI.CreateGridSpacingAttr().Set(fluidRestOffset * 1.5)
+            # isosurfaceAPI.CreateSurfaceDistanceAttr().Set(fluidRestOffset * 1.6)
+            # isosurfaceAPI.CreateGridFilteringPassesAttr().Set("")
+            # isosurfaceAPI.CreateGridSmoothingRadiusAttr().Set(fluidRestOffset * 2)
+
+            # isosurfaceAPI.CreateNumMeshSmoothingPassesAttr().Set(1)
+
+            # primVarsApi = UsdGeom.PrimvarsAPI(particle_system)
+            # primVarsApi.CreatePrimvar("doNotCastShadows", Sdf.ValueTypeNames.Bool).Set(True)
+
+            # self.stage.SetInterpolationType(Usd.InterpolationTypeHeld)
+
             # Create Grid
             gridSpacing = self.cfg.particleSpacing + 0.001
-            lower = self.lower_pos + Gf.Vec3f(-gridSpacing*self.cfg.numParticlesX/2, -gridSpacing*self.cfg.numParticlesY/2, 0)  # Translate lower corner
+            lower = self.lower_pos + Gf.Vec3f(-gridSpacing*self.cfg.numParticlesX/2, -gridSpacing*self.cfg.numParticlesY/2, 0) # Translate lower corner
             positions, velocities = particleUtils.create_particles_grid(
                 lower, gridSpacing, self.cfg.numParticlesX, self.cfg.numParticlesY, self.cfg.numParticlesZ
             )
@@ -110,20 +156,34 @@ class FluidObject():
             # visibility_attribute = self.particlesPrim.GetVisibilityAttr()
             # visibility_attribute.Set("invisible")
 
-            pass
+            # Saves the particles' initial state
+            self.initial_particles_pos = self.get_particles_position(env_index)
 
 
-    def get_particles_position(self, env_id: int)->tuple[np.array, np.array]:
-        # Gets particles' positions in the input environment and velocities and outputs them as arrays
+    def get_particles_position(self, env_id: int = 0) -> torch.Tensor:
+        # Gets particles' positions in the input environment and velocities and outputs them as torch tensors
 
         particles = UsdGeom.Points(self.stage.GetPrimAtPath(self.default_prim_path.AppendPath(f"envs/env_{env_id}/particles")))
-        particles_pos = np.array(particles.GetPointsAttr().Get())
-        particles_vel = np.array(particles.GetVelocitiesAttr().Get())
+        particles_pos = torch.from_numpy(np.asarray(particles.GetPointsAttr().Get())).cuda()
 
-        return particles_pos, particles_vel
+        return particles_pos
+    
+    def get_particles_velocity(self, env_id: int = 0) -> torch.Tensor:
+        # Gets particles' velocities in the input environment and outputs them as array -> torch.tensor
 
-    def set_particles_position(self, particles_pos: np.array, particles_vel: np.array, env_id:int):
-        # Sets the particles' position and velocities to the given arrays
+        particles = UsdGeom.Points(self.stage.GetPrimAtPath(self.default_prim_path.AppendPath(f"envs/env_{env_id}/particles")))
+        particles_vel = torch.from_numpy(np.asarray(particles.GetVelocitiesAttr().Get())).cuda()
+
+        return particles_vel
+
+    def set_particles_position(self, particles_pos: torch.tensor, particles_vel: Union[torch.tensor,None] = None, env_id: int = 0):
+        # Sets the particles' positions to the given array. Velocity set as zero by default
         particles = UsdGeom.Points(self.stage.GetPrimAtPath(self.default_prim_path.AppendPath("envs/env_%d/particles" % env_id)))
-        particles.GetPointsAttr().Set(Vt.Vec3fArray.FromNumpy(particles_pos))
-        particles.GetVelocitiesAttr().Set(Vt.Vec3fArray.FromNumpy(particles_vel))
+        particles.GetPointsAttr().Set(Vt.Vec3fArray.FromNumpy(particles_pos.cpu().numpy()))
+
+        # Resets velocities if given
+        if particles_vel is not None:
+            particles.GetVelocitiesAttr().Set(Vt.Vec3fArray.FromNumpy(particles_vel.cpu().numpy()))
+        else:
+            particles.GetVelocitiesAttr().Set(Vt.Vec3fArray.FromNumpy(np.zeros_like(particles_pos.cpu().numpy())))
+
